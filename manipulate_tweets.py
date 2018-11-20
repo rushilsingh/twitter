16e8d830fdcfd54d1166e0cddfbe185cdd11453c
Rushil Singh <rushilsingh@gmail.com>
2018-11-20 18:48:51 +0530

    Added manipulate tweet script supporting unlike action for now

---
+++ manipulate_tweets.py
@@ -0,0 +1,41 @@
+import twitter
+
+protected_likes = []
+protected_tweets = []
+protected_replies = []
+
+def manipulate(api, action):
+    if action=="unlike":
+        tweets = api.GetFavorites()
+    tweet_ids = [tweet.id for tweet in tweets]
+    try:
+            if action =="unlike":
+                    protected = protected_likes
+            count = 0
+            for tweet_id in tweet_ids:
+                    if tweet_id in protected:
+                        print "Protected tweet encountered"
+                        continue
+                    try:
+                        if action == "unlike":
+                            api.DestroyFavorite(status_id=tweet_id)
+                    except Exception as e: print e
+                    count += 1
+                    time.sleep(0.5)
+    except:
+            pass
+
+    print "Number of tweets: %s\n" % count
+
+def main():
+
+    api = twitter.Api(consumer_key="",
+                      consumer_secret="",
+                      access_token_key="",
+                      access_token_secret="")
+
+    manipulate(action, "unlike")
+
+
+if __name__ == "__main__":
+    main()
