4d4960169cbafc5534b5eb642808beda2984fa9c
Rushil Singh <rushilsingh@pm.me>
2018-12-14 18:15:07 +0530

    Fixed indentation issue

--- manipulate_tweets.py
+++ manipulate_tweets.py
@@ -12,16 +12,16 @@ def manipulate(api, action, option):
         tweets = api.GetReplies(count=1000)
         tweet_ids = [str(tweet.id) for tweet in tweets if tweet.favorite_count == 0 and tweet.in_reply_to_screen_name not in users]
 
-        try:
-            print(action)
-            if action == "unlike":
-                api.DestroyFavorite(status_id=tweet_id)
-            elif action == "delete":
-                api.DestroyStatus(status_id=tweet_id)
-            count += 1
-        except Exception as e:
-            print(e.message)
-        time.sleep(0.5)
+    try:
+        print(action)
+        if action == "unlike":
+            api.DestroyFavorite(status_id=tweet_id)
+        elif action == "delete":
+            api.DestroyStatus(status_id=tweet_id)
+        count += 1
+    except Exception as e:
+        print(e.message)
+    time.sleep(0.5)
 
     print("Number of tweets %sd: %s\n" % (count, action))
 
