172b095826bfb7b2b4a82e9bca4a257bcc0fd8a7
Rushil Singh <rushilsingh@pm.me>
2018-12-14 18:10:42 +0530

    Minor refactoring

--- manipulate_tweets.py
+++ manipulate_tweets.py
@@ -12,21 +12,18 @@ def manipulate(api, action, option):
         tweets = api.GetReplies(count=1000)
         tweet_ids = [str(tweet.id) for tweet in tweets if tweet.favorite_count == 0 and tweet.in_reply_to_screen_name not in users]
 
-    try:
-                try:
-                    print(action)
-                    if action == "unlike":
-                        api.DestroyFavorite(status_id=tweet_id)
-                    elif action == "delete":
-                        api.DestroyStatus(status_id=tweet_id)
-                except Exception as e:
-                    print(e.message)
-                count += 1
-                time.sleep(0.5)
-    except:
-        pass
+        try:
+            print(action)
+            if action == "unlike":
+                api.DestroyFavorite(status_id=tweet_id)
+            elif action == "delete":
+                api.DestroyStatus(status_id=tweet_id)
+            count += 1
+        except Exception as e:
+            print(e.message)
+        time.sleep(0.5)
 
-    print("Number of tweets: %s\n" % count)
+    print("Number of tweets %sd: %s\n" % (count, action))
 
 def main():
 
