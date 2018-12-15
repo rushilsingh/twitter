a0c6c9e8622cf9abe6d95b2fe5da603858f1c12b
Rushil Singh <russingh@contractor.ciena.com>
2018-12-15 22:34:34 +0530

    Fixed bugs

--- manipulate_tweets.py
+++ manipulate_tweets.py
@@ -7,23 +7,24 @@ def manipulate(api, action):
     users = [user.screen_name for user in users]
     if action=="unlike":
         tweets = api.GetFavorites(count=1000)
-        tweet_ids = [str(tweet.id) for tweet in tweets if tweet.screen_name not in users]
+        tweet_ids = [str(tweet.id) for tweet in tweets if tweet.user.screen_name not in users]
     elif action == 'delete':
         tweets = api.GetReplies(count=1000)
         tweet_ids = [str(tweet.id) for tweet in tweets if tweet.favorite_count == 0 and tweet.in_reply_to_screen_name not in users]
 
     try:
-        print(action)
+        count = 0
         if action == "unlike":
-            api.DestroyFavorite(status_id=tweet_id)
+            [api.DestroyFavorite(status_id=tweet_id) for tweet_id in tweet_ids]
+            count = len(tweet_ids)
         elif action == "delete":
-            api.DestroyStatus(status_id=tweet_id)
-        count += 1
+            [api.DestroyStatus(status_id=tweet_id) for tweet_id in tweet_ids]
+            count = len(tweet_ids)
     except Exception as e:
         print(e.message)
     time.sleep(0.5)
 
-    print("Number of tweets %sd: %s\n" % (count, action))
+    print("Number of tweets %sd: %s\n" % (action, count))
 
 def main():
 
